3389a802c3674db25953e466c1e1bab267450678
Phạm Đức Thanh <phamducthanh@MacBook-Pro-cua-Pham.local>
2024-03-24 11:52:48 +0900

    Dự án đầu tiên

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+var server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html")  // mặc định là html
+    // res.setHeader("Content-Type", "text/plain");
+    // res.setHeader("Content-Type", "application/json"); // viết API dùng nhiều hơn
+    res.end("<h1>Hello NodeJS</h1>")
+});
+
+server.listen(8080, () => {
+    console.log("Server listening http://localhost:8080");
+});
